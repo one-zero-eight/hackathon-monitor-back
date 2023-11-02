2b7471d410e7f9b3d5b15af50d7afddd0deb42b5
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-11-02 23:50:56 +0300

    Initial commit

---
+++ src/schemas/__init__.py
@@ -0,0 +1,12 @@
+# fmt: off
+__all__ = [
+    "CreateSomeScheme", "ViewSomeScheme", "UpdateSomeScheme",
+]
+
+# fmt: on
+
+from src.schemas.some_scheme import (
+    CreateSomeScheme,
+    ViewSomeScheme,
+    UpdateSomeScheme,
+)
